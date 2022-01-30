284044afea8b385d5baae98adc0e162a6dcdf2ee
Rudiansyah <69573307+rudiansyahirun@users.noreply.github.com>
2022-01-30 09:23:54 +0700

    Update Employee.java
    commit base

--- src/Employee.java
+++ src/Employee.java
@@ -1,22 +1,29 @@
 public class Employee {
     public Employee(String name,double baseSalary) {
-
+        // Construct Employee with two parameters name and base salary
     }
     public double getBaseSalary() {
-        return baseSalary;
+        // Should return the employee's base salary
     }
     public String getName() {
-        return name;
+        // Should return the employee's name
     }
     public int getEmpolyeeID() {
-        return empolyeeID;
+        // Should return the employee's ID.
+        /*
+        The ID should be issued on behalf of the employee at the
+        time they are constructed. The first employee should have
+        an ID of "1", the second "2" and so on.
+        */
     }
     public Employee getManager() {
-        return manager;
+        /*
+        Should return a reference to the Employee object that
+        represents this employee's manager
+        */
     }
     public boolean equals(Employee other) {
         // should return true if the two employee's ID are the same, false otherwise
-
     }
     public String toString() {
         /*
@@ -24,13 +31,11 @@ public class Employee {
         is a combination of their id followed by their name.
         Example: "1 Kasey"
         */
-
     }
     public String employeeStatus() {
         /*
         Should return a String representation of that Employee's
         current status. This will be different for every subclass of Employee
         */
-
     }
 }
